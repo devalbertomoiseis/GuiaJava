86a24735a20f7e11de05a91844d2a4176e8106ff
albertomoiseis <contatoalbertomoiseis@gmail.com>
2022-07-12 19:50:24 -0300

    Projeto: Mostrando o Segundo Programa na Tela.

---
+++ src/fundamentos/com/br/Comentarios.java
@@ -0,0 +1,19 @@
+package fundamentos.com.br;
+/*
+*
+* @author albertomoiseis
+* @since JDK8.0
+* */
+public class Comentarios {
+    /*
+    * @param args
+    * */
+    public static void main(String[] args) {
+        // Sentença de Código termina ;
+        // Mais uma linha...
+        // Fim
+
+        System.out.println("Primeiro Programa Parte #01");
+
+    }
+}
