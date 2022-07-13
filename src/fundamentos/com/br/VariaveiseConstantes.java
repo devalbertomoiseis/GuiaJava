15689ad8a909bafd7b2a035a21ede4a5fe0167aa
albertomoiseis <contatoalbertomoiseis@gmail.com>
2022-07-12 22:59:56 -0300

    Projeto: Trabalhando com Váriaveis e Constantes

---
+++ src/fundamentos/com/br/VariaveiseConstantes.java
@@ -0,0 +1,12 @@
+package fundamentos.com.br;
+
+public class VariaveiseConstantes {
+    public static void main(String[] args) {
+
+        double raio = 3.4;
+        final double PI = 3.14159;
+        double area = PI * raio * raio;
+        System.out.println("Valor da Area: " + area);
+
+    }
+}
