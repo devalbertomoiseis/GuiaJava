2584cd86faa6241c963b3e14a54de1a70635196a
albertomoiseis <contatoalbertomoiseis@gmail.com>
2022-07-14 00:12:08 -0300

    Projeto: Trabalhando com Váriaveis e Constantes

---
+++ src/fundamentos/com/br/DesafioVariaveisConstantes.java
@@ -0,0 +1,13 @@
+package fundamentos.com.br;
+
+public class DesafioVariaveisConstantes {
+    public static void main(String[] args) {
+
+        final double FATOR = 5.0 / 9.0;
+        final double AJUSTE = 32;
+
+        double fahrenheit = 86;
+        double celsius = (fahrenheit - AJUSTE) * FATOR;
+        System.out.println("O resultado é " + celsius + "°C.");
+    }
+}
